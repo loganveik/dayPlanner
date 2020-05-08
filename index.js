36ebb34f7bd897e13e8247e7d7671c19a5666a85
ljv29 <jumpswervelogan@gmail.com>
2020-05-08 01:57:25 -0700

    created css for initial html, and js for moment and changing classes based on time of day

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-moment().format();
+const m = moment();
+const displayCurrentTimeAndDay = function () {
+    const currentday = m.format("LLL");
+    console.log(m.hour())
+    $("#currentdayview").append(currentday);
+}
+displayCurrentTimeAndDay();
+
+
+const currentTime = parseInt(m.format("HH00"), 10);
+for (let i = 0; i < $(".hour").length; i++) {
+    let currentDiv = $($(".hour")[i]).next();
+    let dataTime = parseInt($($(".hour")[i]).data().time, 10);
+    if (dataTime < currentTime) {
+        currentDiv.addClass("past");
+    } else if (dataTime > currentTime) {
+        currentDiv.addClass("future");
+    } else {
+        currentDiv.addClass("present");
+    }
+}
+
+$(".saveBtn").on("click", function () {
+    const userEvent = $(this).prev().val();
+    const btn = $(this).val()
+    localStorage.setItem(btn, userEvent);
+    render();
+})
+
+function render() {
+    for (let i = 0; i < 9; i++) {
+        const oneEvent = localStorage.getItem(i);
+        $("#" + i).text(oneEvent);
+    }
+}
+
+$(document).ready(render());
